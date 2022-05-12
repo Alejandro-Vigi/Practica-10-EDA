a53abe2322cdfd3201cfb22829193a1482269ebb
Alejandro-Vigi <marco.vigi@ingenieria.unam.edu>
2022-05-11 20:54:48 -0500

    Corrigiendo archivo graficas.py

--- programas/graficas.py
+++ programas/graficas.py
@@ -1,13 +1,12 @@
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3D
-import math
-
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 from numpy import linspace
 
 x = linspace(0, 5, 20)
-
+y = np.sin(x)
 fig, ax = plt.subplots(facecolor='w', edgecolor='k')
-ax.plot(x, math.sin(x), marker="o", color="r", linestyle="None")
+ax.plot(x, y, marker="o", color="r", linestyle="None")
 
 ax.grid(True)
 ax.set_xlabel("X")
